a0b353490f130847ccec6bad1b78c4a8c784c606
Moamiine <medamine.teljaoui@gmail.com>
2025-12-25 14:34:27 +0100

    created formateur & admin classes

--- Repository/utilisateurRepository.php
+++ Repository/utilisateurRepository.php
@@ -1,10 +1,4 @@
 <?php 
-require '../Abstract/Person.php';
-
-require '../Database/DatabaseConnection.php';
-include '../Entity/utilisateur.php';
-
-
 
   $user = new Utilisateur(
     null, 
--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php 
-include 'Repository/utilisateurRepository.php';
+include_once 'Abstract/Person.php';
+include_once 'Entity/utilisateur.php';
+include_once 'Entity/Admnistrateur.php';
+include_once 'Entity/Formateur.php';
+include_once 'Repository/utilisateurRepository.php';
